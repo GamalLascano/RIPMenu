6aa6e50355bc06631c5f038ede6ac8e702763d0b
Gamal Lascano <gamallascano@gmail.com>
2019-11-19 17:18:46 -0300

    ClockOSD changes

---
+++ ClockOSD.c
@@ -0,0 +1,18 @@
+#include "sapi.h" 
+void mostrarOSD(char * strMoisture){
+	lcdGoToXY( 0, 0 ); 
+	lcdData(0);
+	lcdSendStringRaw( "HUM: " );
+	lcdSendStringRaw( strMoisture );
+	lcdSendStringRaw( "%" );
+}
+void showAuto(void){
+	lcdGoToXY( 0, 1 );
+	lcdSendStringRaw( "AU: " );
+	lcdSendStringRaw( "00:00" );
+}
+void showTimer(void){
+	lcdGoToXY( 0, 1 );
+	lcdSendStringRaw( "TM: " );
+	lcdSendStringRaw( "00:00" );
+}
--- menuMEF.c
+++ menuMEF.c
@@ -1,5 +1,5 @@
 #include "sapi.h" 
-
+#include "ClockOSD.h" 
 enum menuState {OSD,MENU,DIRECT,TIMER,AUTO};
 menuState estado;
 const char humChar[8] = { 
@@ -12,18 +12,118 @@ const char humChar[8] = {
    0b01110,
    0b00000
 };
+#define BUTTON_LOGIC BUTTON_ONE_IS_UP
+#if BOARD==edu_ciaa_nxp
+	#define BUTTON0 TEC1
+	#define BUTTON1 TEC2
+	#define BUTTON2 TEC3
+	#define BUTTON3 TEC4
+#else
+	#error You must select a valid board!
+#endif
+uint16_t SoilSensor;
+float moist;
+char * medida;
+uint32_t osdint = 0;
 int main (void){
 	boardConfig();
 	estado=OSD;
 	lcdInit( 16, 2, 5, 8 );
+	lcdCursorSet( LCD_CURSOR_OFF );
 	lcdCreateChar( 0, humChar );
 	adcConfig( ADC_ENABLE );
+	delay_t refreshButton;
+    delay_t refreshButtonEvents;
+    delayInit( &refreshButton, 20 );
+    delayInit( &refreshButtonEvents, 10 );
+    button_t boton0;
+    button_t boton1;
+    button_t boton2;
+    button_t boton3;
+	buttonInit( &boton0,                  // Button structure (object)
+               BUTTON0, BUTTON_LOGIC,       // Pin and electrical connection
+               50,                          // Button scan time [ms]
+               TRUE,                        // checkPressedEvent
+               FALSE,                       // checkReleasedEvent
+               TRUE,                        // checkHoldPressedEvent
+               5000,                        // holdPressedTime [ms]
+               0,                           // pressedCallback
+               0,                           // releasedCallback
+               0                            // holdPressedCallback
+    );
+	buttonInit( &boton1,                  // Button structure (object)
+               BUTTON1, BUTTON_LOGIC,       // Pin and electrical connection
+               50,                          // Button scan time [ms]
+               TRUE,                        // checkPressedEvent
+               FALSE,                       // checkReleasedEvent
+               TRUE,                        // checkHoldPressedEvent
+               5000,                        // holdPressedTime [ms]
+               0,                           // pressedCallback
+               0,                           // releasedCallback
+               0                            // holdPressedCallback
+    );
+	buttonInit( &boton2,                  // Button structure (object)
+               BUTTON2 BUTTON_LOGIC,       // Pin and electrical connection
+               50,                          // Button scan time [ms]
+               TRUE,                        // checkPressedEvent
+               FALSE,                       // checkReleasedEvent
+               TRUE,                        // checkHoldPressedEvent
+               5000,                        // holdPressedTime [ms]
+               0,                           // pressedCallback
+               0,                           // releasedCallback
+               0                            // holdPressedCallback
+    );
+	buttonInit( &boton3,                  // Button structure (object)
+               BUTTON3, BUTTON_LOGIC,       // Pin and electrical connection
+               50,                          // Button scan time [ms]
+               TRUE,                        // checkPressedEvent
+               FALSE,                       // checkReleasedEvent
+               TRUE,                        // checkHoldPressedEvent
+               5000,                        // holdPressedTime [ms]
+               0,                           // pressedCallback
+               0,                           // releasedCallback
+               0                            // holdPressedCallback
+    );  
 	while(TRUE){
 		SoilSensor = adcRead( CH1 );
+		moist = SoilSensor / 10.23;
+		moist = 100 - moist;
+		myftoa(moist,medida,0);
+		if( delayRead(&refreshButton) ) {
+         buttonFsmUpdate( &boton0 );
+         buttonFsmUpdate( &boton1 );
+         buttonFsmUpdate( &boton2 );
+         buttonFsmUpdate( &boton3 );
+        }
 		switch(estado){
 			case OSD:
+				lcdClear(); 
+				mostrarOSD(medida);
+				if( delayRead(&refreshButtonEvents) ) {
+					if((buttonEventGet( &boton1 ) == BUTTON_PRESSED)||(buttonEventGet( &boton2 ) == BUTTON_PRESSED)){
+						if (buttonEventGet( &boton1 ) == BUTTON_PRESSED){
+							buttonEventHandled( &boton1 );
+						} else{
+							buttonEventHandled( &boton2 );
+						}
+						if (osdint == 0){
+							osdint = 1;
+						}else osdint = 0;
+					}
+					if (osdint==0){
+						showAuto();
+					}else{
+						showTimer();
+					}
+					if (buttonEventGet( &boton3 ) == BUTTON_PRESSED){
+						buttonEventHandled( &boton3 );
+						estado = MENU;
+					}
+				}
+				delay(40);
 			break;
 			case MENU:
+
 			break;
 			case DIRECT:
 			break;
